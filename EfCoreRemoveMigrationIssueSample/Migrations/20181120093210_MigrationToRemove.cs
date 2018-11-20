90e0a03d1f89996790eb0a87cbd2a5885b1108c4
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2018-11-20 10:32:34 +0100

    Add MigrationToRemove

---
+++ EfCoreRemoveMigrationIssueSample/Migrations/20181120093210_MigrationToRemove.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCoreRemoveMigrationIssueSample.Migrations
+{
+    public partial class MigrationToRemove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
